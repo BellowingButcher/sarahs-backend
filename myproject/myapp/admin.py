3220811a5cfaa668ff356e5cf4025de7dfefd7bb
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-11-29 23:29:08 +0000

    fixed the admin site and got tokens to return correctly

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,7 +1,16 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 from .models import CustomUser
-# Register your models here.
-class CustomUserAdmin(admin.ModelAdmin):
-    model = CustomUser
 
+class CustomUserAdmin(UserAdmin):
+    ...
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('is_teamleader', 'is_teammember')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('is_teamleader', 'is_teammember')}),
+    )
+
+
+# Now register the new UserAdmin...
 admin.site.register(CustomUser, CustomUserAdmin)
