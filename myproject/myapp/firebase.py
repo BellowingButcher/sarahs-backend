030b71473e73c808d7f6cdee911081576e38f9d8
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-12-01 23:09:02 +0000

    upload schedule buttons are functional

---
+++ myproject/myapp/firebase.py
@@ -0,0 +1,17 @@
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyBVnXXEUy3ujtvX_m7ldMnVk3gxK3AdirA",
+  "authDomain": "total-time-tracker.firebaseapp.com",
+  "databaseURL": "gs://total-time-tracker.appspot.com",
+  "storageBucket": "total-time-tracker.appspot.com",
+  # seviceAccount this allows us to bypass authenticating users on this end.
+  # I plan to do that on the front end with a log in process
+  "serviceAccount": "path/to/serviceAccountCredentials.json",
+}
+
+
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
+print(db)
