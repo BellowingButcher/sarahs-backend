45596a0d736c695d856057297f0e1e5c88405fd5
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-12-08 18:31:48 +0000

    It is working!
    - make sure to structure the file names that are being stored properly
    - fileName.(whatEverFileTypeItIs)

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -10,6 +10,7 @@ from .firebase import storage
 import numpy as np
 import pandas as pd
 from django.utils.timezone import make_aware
+# import glob
 # from .firebase import firebase
 
 
@@ -22,25 +23,27 @@ class SaveSchedule(generics.ListCreateAPIView):
     queryset = Schedules.objects.all()
     serializer_class = SchedulesSerializer
 
-
     def create(self, request):
+
         df = pd.read_excel(request.data["schedule"], header=None)
+
         # print(df.iloc[6,3])
         data = {
             'schedule': request.data["schedule"],
             'uploaded_by': request.user.id,
             'beginning': df.iloc[6, 3],
-            'ending': df.iloc[6, 8],
+            'ending': df.iloc[6, 9],
             'status': "True",
         }
         serializer = SchedulesSerializer(data=data)
         if serializer.is_valid():
-            naive_beginning =  df.iloc[6, 3]
+            naive_beginning = df.iloc[6, 3]
             naive_ending = df.iloc[6, 8]
             aware_beginning = make_aware(naive_beginning)
             aware_ending = make_aware(naive_ending)
             try:
-                obj = Schedules.objects.get(beginning=aware_beginning, ending=aware_ending,)
+                obj = Schedules.objects.get(
+                    beginning=aware_beginning, ending=aware_ending,)
                 return Response(
                     "Schedule already exists"
                 )
